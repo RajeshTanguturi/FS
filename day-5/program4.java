30b1d99e63507f316c98b70ab6a4cba07ef2cb79
Rajesh <rajeshtanguturi31@gmail.com>
2025-02-13 14:35:39 +0530

    done day4 program 3

--- day-5/program4.java
+++ day-5/program4.java
@@ -1,46 +1,49 @@
-// not done 
-// Construct Tree from the given Level-order and In-order.
-// Compute the Depth and Sum of the Deepest nodes in the Binary tree
+/*
+ 
+ 
+Construct Tree from the given Level-order and In-order.
+Compute the Depth and Sum of the Deepest nodes in the Binary tree
 
-// Input Format:
-// -------------
-// An integer N representing the number of nodes.
-// A space-separated list of N integers representing the in-order traversal.
-// A space-separated list of N integers representing the level-order traversal.
+Input Format:
+-------------
+An integer N representing the number of nodes.
+A space-separated list of N integers representing the in-order traversal.
+A space-separated list of N integers representing the level-order traversal.
 
-// Output Format:
-// --------------
-// Print two values:
-// ->The maximum number of levels.
-// ->The sum of all node values at the deepest level.
+Output Format:
+--------------
+Print two values:
+->The maximum number of levels.
+->The sum of all node values at the deepest level.
 
-// Example:
-// -------------
-// Input:
-// 11
-// 7 8 4 2 5 9 11 10 1 6 3
-// 1 2 3 4 5 6 7 9 8 10 11
+Example:
+-------------
 
-// Output:
-// 6 11
+Input:
+11
+7 8 4 2 5 9 11 10 1 6 3
+1 2 3 4 5 6 7 9 8 10 11
 
-// Explanation:
-// The binary tree structure:
-//            1
-//          /   \
-//        2       3
-//       / \     /
-//      4   5   6
-//     /     \   
-//    7       9
-//     \       \
-//      8      10
-//             /
-//           11
-// Maximum Depth: 6
-// nodes at the Deepest Level (6): 11
-// Sum of nodes at Level 6: 11
+Output:
+6 11
 
+Explanation:
+The binary tree structure:
+           1
+         /   \
+       2       3
+      / \     /
+     4   5   6
+    /     \   
+   7       9
+    \       \
+     8      10
+            /
+          11
+Maximum Depth: 6
+nodes at the Deepest Level (6): 11
+Sum of nodes at Level 6: 11
+*/
 import java.util.*;
 
 class Node{
@@ -54,63 +57,63 @@ class Node{
     }
 }
 public class program4 {
-    public static Node rec(int[] preorder, int[] inorder,Map<Integer,Integer> inorderIndex , int sidx, int eidx, int preIdx[]){
-        if(sidx > eidx) return null;
-        Node root = new Node(preorder[preIdx[0]++]);
-        int rootIndex = inorderIndex.get(root.val);
-        root.left = rec(preorder, inorder, inorderIndex, sidx, rootIndex-1,preIdx);
-        root.right = rec(preorder, inorder, inorderIndex, rootIndex+1, eidx,preIdx);
+    public static PriorityQueue<Integer> buildPQ(int start, int end,int[] inorder,Map<Integer,Integer> levelorderMap){
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+            int idx1 = levelorderMap.get(a);
+            int idx2 = levelorderMap.get(b);
+            return idx1 - idx2;
+        });
+        for (int i = start; i <= end; i++) {
+            pq.offer(inorder[i]);
+        }
+        return pq;
+    } 
+    public static Node buildTree( int[] inorder,Map<Integer,Integer> inorderMap,Map<Integer,Integer> levelorderMap , int sidx, int eidx,int curEle){
+        Node root = new Node(curEle);
+        int rootIndex = inorderMap.get(curEle);
+        PriorityQueue<Integer> leftSubTreeElements = buildPQ(sidx,rootIndex-1,inorder,levelorderMap);
+        PriorityQueue<Integer> rightSubTreeElements = buildPQ(rootIndex+1,eidx,inorder,levelorderMap);
+
+        if(leftSubTreeElements.isEmpty()){
+            root.left = null;
+        }else{
+
+            int leftchildEle = leftSubTreeElements.poll();
+            root.left = buildTree( inorder, inorderMap,levelorderMap, sidx, rootIndex-1,leftchildEle);
+        }
+        if (rightSubTreeElements.isEmpty()) {
+            root.right = null;
+        }else{
+            int rightchildEle = rightSubTreeElements.poll(); 
+            root.right = buildTree( inorder, inorderMap,levelorderMap, rootIndex+1, eidx,rightchildEle);
+        }
         return root;
     }
-    public static List<List<Integer>> levelOrder(Node root){
-        List<List<Integer>> res = new ArrayList<>();
-        Queue<Node> que = new LinkedList<>();
-        if(root==null) return res;
-        que.add(root);
-        int level = 1;
-        while(!que.isEmpty()){
-            List<Integer> sub = new ArrayList<>();
-            int size = que.size();
-            for(int i=0;i<size;i++){
-                Node curr = que.poll();
-                sub.add(curr.val);
-                if(curr.left!=null) que.add(curr.left);
-                if(curr.right!=null) que.add(curr.right);
-            }
-            if(level % 2 == 0){
-                Collections.reverse(sub);
-                res.add(sub);
-            }else{
-                res.add(sub);
-            }
-            level++;
-        }
-        return res;
+    public static void inorder (Node root){
+        if( root == null) return;
+        inorder(root.left);
+        System.out.println(root.val);
+        inorder(root.right);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int inorder[] = new int[n];
-        int preorder[] = new int[n];
-        int preIdx[] = new int[]{0};
-        Map<Integer,Integer> inorderIndex = new HashMap<>();
+        int levelorder[] = new int[n];
+        Map<Integer,Integer> inorderMap = new HashMap<>();
+        Map<Integer,Integer> levelorderMap = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             inorder[i] = sc.nextInt();
-            inorderIndex.put(inorder[i],i);
+            inorderMap.put(inorder[i],i);
         }
         for (int i = 0; i < n; i++){
-            preorder[i] = sc.nextInt();
+            levelorder[i] = sc.nextInt();
+            levelorderMap.put(levelorder[i], i);
         }
-        int l = sc.nextInt();
-        int r = sc.nextInt();
         
-        Node root = rec(preorder,inorder,inorderIndex,0,n-1,preIdx);
-        List<List<Integer>> ansl = levelOrder(root);
-        ArrayList<Integer> res = new ArrayList<>();
-        // System.out.println(ansl);
-        for( int i = l ; i<=r ; i++){
-            res.addAll(ansl.get(i-1));
-        }
-        System.out.println(res);
+        Node root = buildTree(inorder,inorderMap,levelorderMap,0,n-1,levelorder[0]);
+        inorder(root);
+
     }
 }
--- temp.java
+++ temp.java
@@ -1,13 +1,8 @@
 import java.util.*;
 public class temp {
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<>();
-        arr.add(1);
-        arr.add(0);
-        arr.add(2);
-        System.out.println(arr);
-        arr.remove(Integer.valueOf(0));
-        System.out.println(arr);
+        int x = 00000000005;
+        System.out.println(x == 5);
 
     }
 }
